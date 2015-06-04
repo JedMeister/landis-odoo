8adbc89738e90f0b2d23cdc1d6fc28c7065b19ed
L. Arnold <larnold@nomadic.net>
2015-06-04 13:28:51 -0600

    Create odoo.py
    for openerp branch

---
+++ overlay/usr/lib/inithooks/bin/odoo.py
@@ -0,0 +1,13 @@
+
+/usr/lib/inithooks/firstboot.d/50admin-db-pass
+
+#!/bin/bash -e
+# set odoo db password
+
+. /opt/openerp/odoo/openerp-server.conf
+
+updateconf() {
+    CONF=/opt/openerp/odoo/openerp-server.conf
+
+    sed -i "s/#db_password = openuser / db_password = ${OPENERP_PASS}" $CONF
+}
